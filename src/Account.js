b0a2eaa7d69e7e91be3a1ac818e1400fb6a0e65b
heyjulieh <heyjulieh@gmail.com>
2017-05-01 16:36:05 -0700

    withdraw and zero styling works

--- src/Account.js
+++ src/Account.js
@@ -18,18 +18,36 @@ export default class Account extends Component {
       this.setState({
         balance: newBalance
       })
-
       amount = '';
-      // this.handleDepClick = this.handleDepClick.bind(this)
     }
+
+    handleWithClick (event) {
+        // It is good practice to still prevent default behavior
+        event.preventDefault()
+        // set a local variable to the amount entered in the text box.
+        let amount = parseInt(this.refs.amount.value)
+        // set a local variable to the new balance based off of the original balance + amoun
+        let newBalance = this.state.balance - amount;
+        // set the balance to the newBalance using the setState method (necessary)
+        this.setState({
+          balance: newBalance
+        })
+        amount = '';
+      }
+
   render() {
+    let balance = "balance";
+    if (this.state.balance <=0){
+      balance += ' zero';
+      balance
+    }
     return (
       <div className="account">
         <h2>{this.props.name}</h2>
-        <div className="balance">{this.state.balance}</div>
+        <div className={balance}>{this.state.balance}</div>
           <input type="text" placeholder="enter an amount" ref="amount" />
         <input type="button" value="Deposit" onClick={(event)=>this.handleDepClick(event)}/>
-        <input type="button" value="Withdraw" />
+        <input type="button" value="Withdraw" onClick={(event)=>this.handleWithClick(event)}/>
       </div>
     )
   }
